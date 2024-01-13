2cd3fdea6a8c689b22e6ffac565313ed0fbe7a97
Shenya De Silva <shenya44@outlook.com>
2024-01-13 22:16:44 +0100

    Added hook for esc key

--- main.c
+++ main.c
@@ -10,6 +10,21 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+int	close(int keycode, t_vars *vars)
+{
+	if (keycode == 53)
+	{
+		mlx_destroy_window(vars->mlx, vars->win);
+		exit(0);
+	}
+	return (0);
+}
+
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
@@ -20,24 +35,23 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 int mlx(void)
 {
-	void	*mlx = NULL;
-	void	*mlx_win = NULL;
+	t_vars vars;
 	t_data 	img;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 800, 500, "Hello world!");
-	img.img = mlx_new_image(mlx, 800, 500);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 800, 500, "Hello world!");
+	img.img = mlx_new_image(vars.mlx, 800, 500);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
 	my_mlx_pixel_put(&img, 100, 100, 0x00FFFFFF);
 	my_mlx_pixel_put(&img, 200, 100, 0x00FFFFFF);
 	my_mlx_pixel_put(&img, 200, 200, 0x00FFFFFF);
 	my_mlx_pixel_put(&img, 100, 200, 0x00FFFFFF);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	
-	mlx_loop(mlx);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+	mlx_hook(vars.win, 2, 1L<<0, close, &vars);
+	mlx_loop(vars.mlx);
 	printf("MLX Closed.\n");
-	free(mlx_win);
+	mlx_destroy_window(vars.mlx, vars.win);
 	return (0);
 }
 
